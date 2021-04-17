5589b6652a1ad61f435681e4a444b1c3fa91555f
unknown <you@example.com>
2021-04-18 01:57:27 +0600

    Resolved faild deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  
 } from "react-router-dom";
 import AllTeams from './Components/AllTeams/AllTeams';
 import NotFound from './Components/NotFound/NotFound';
